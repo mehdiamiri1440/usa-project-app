7ec1007343858ca58f8558b96c5f616c781d7a43
mehdi amiri <knownmma@gmail.com>
2020-01-29 17:27:47 +0330

    auth redux created and connect with fake api call

---
+++ src/redux/auth/actions.js
@@ -0,0 +1,24 @@
+import {
+    action,
+    generateErrorAction
+} from '../actions';
+import actionTypes from './actionTypes';
+
+export const login = (mobileNumber, password) => {
+    const request = () => {
+        return dispatch => {
+            dispatch(loading());
+            return API.auth
+                .login(mobileNumber, password)
+                .then(res => dispatch(success(res)))
+                .catch(err => {
+                    dispatch(generateErrorAction(err, { failure: actionTypes.LOGIN_FAILURE, reject: actionTypes.LOGIN_REJECT }));
+                    throw err;
+                });
+        };
+    };
+    const loading = () => action(actionTypes.LOGIN_LOADING);
+    const success = res => action(actionTypes.LOGIN_SUCCESS, res.payload);
+
+    return request();
+};
