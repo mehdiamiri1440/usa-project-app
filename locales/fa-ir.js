8cdb46bd1ab3c2cdf46a861938809f4aceaeb5d2
mehdi amiri <knownmma@gmail.com>
2020-02-04 17:10:15 +0330

    localizations added

--- locales/fa-ir.js
+++ locales/fa-ir.js
@@ -2,15 +2,20 @@ export default {
   locale: 'fa-ir',
   title: 'فارسی',
   strings: {
+    "errors.mobileOrPasswordIncorrect": " شماره همراه یا کلمه عبور اشتباه است . ",
+
     "titles.enterToBuskool": "ورود به باسکول",
     "titles.phoneNumber": 'شماره همراه',
     "titles.password": 'کلمه عبور',
     "titles.login": 'ورود',
     "titles.signUpInBuskool": "ثبت نام در باسکول",
+    'titles.submitNumber': 'ثبت شماره',
 
 
     "messages.signedUpUser": "اگر کاربر قبلی هستید برای ورود شماره همراه و رمز عبور خود را وارد کنید",
     "messages.forgotPassword": "کلمه عبور خود را فراموش کرده ام",
     "messages.startToSignUp": "برای ثبت نام در باسکول بر روی دکمه زیر کلیک کنید",
+    'messages.enterPhoneNumberToGetCode': 'شماره همراه خود را برای دریافت کد فعال‌سازی وارد کنید',
+    'messages.backToLogin': 'درصورت داشتن  حساب وارد باسکول شوید',
   }
 };
