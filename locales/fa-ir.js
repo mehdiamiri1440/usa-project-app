d89e463b0ae6a286b90de27c4798cff46439ce93
mehdi amiri <knownmma@gmail.com>
2020-02-13 16:19:11 +0330

    user authority step and also choose city done on design and validaiton and api calls some new localizations added to project

--- locales/fa-ir.js
+++ locales/fa-ir.js
@@ -3,22 +3,30 @@ export default {
   title: 'فارسی',
   strings: {
     "errors.mobileOrPasswordIncorrect": " شماره همراه یا کلمه عبور اشتباه است . ",
+    "errors.invalidUserName": "نام کاربری نا‌معتبر است",
+    "errors.passwordLength": "طول کلمه عبور حداقل باید 8 کارکتر باشد",
+    "errors.notSamePasswords": "کلمه عبور و تکرار آن باهم مطابقت ندارند",
     'errors.errorInVerificationCode': 'کد وارد شده صحیح نیست یا منقضی شده است',
 
 
     "labels.man": "آقا",
     "labels.selectProvince": "استان را انتخاب کنید",
     "labels.selectCity": "شهر را انتخاب کنید",
+    "labels.submitUserBasics": "ثبت مشخصات فردی شما",
     "labels.woman": "خانم",
 
 
     "titles.enterToBuskool": "ورود به باسکول",
+    "titles.chooseCityAndProvince": "با وارد کردن شهر، شما می توانید با دیگران بهتر ارتباط برقرار کنید",
     "titles.phoneNumber": 'شماره همراه',
     "titles.password": 'کلمه عبور',
     "titles.login": 'ورود',
+    "titles.repeatPassword": "تکرار کلمه عبور",
+    "titles.userName": "نام کاربری",
     'titles.sendVerificationCodeAgain': 'ارسال مجدد رمز عبور',
     "titles.submitCode": 'ثبت کد',
     "titles.signUpInBuskool": "ثبت نام در باسکول",
+    "titles.nextStep": "مرحله بعد",
     'titles.submitNumber': 'ثبت شماره',
     'titles.firstName': 'نام',
     'titles.lastName': 'نام‌خانوادگی',
