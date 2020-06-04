ca63bbb3910753a4e4ae306cd9cf425786b7f492
mehdi amiri <knownmma@gmail.com>
2020-06-04 14:41:41 +0430

    first loading to show to ali

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { ActivityIndicator, View } from 'react-native';
+import { deviceWidth, deviceHeight } from '../../utils/deviceDimenssions';
 
 const Spin = props => {
     const { spinning = false, children = null, size = 'large', color = 'green' } = props;
-    if (!spinning) return children;
+    // if (!spinning) return children;
 
     return (
-        <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingVertical: 10 }}>
-            <ActivityIndicator animating size={size} color={color} />
-        </View>
+        <>
+            {spinning && <View style={{
+                position: 'absolute', flex: 1, backgroundColor: 'white',
+                opacity: 0.4, zIndex: 999, width: deviceWidth, alignSelf: 'center', height: deviceHeight,
+                flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingVertical: 10
+            }}>
+                <ActivityIndicator animating size={size} color={color} />
+            </View>}
+            {children}
+        </>
     );
 }
 export default Spin;
