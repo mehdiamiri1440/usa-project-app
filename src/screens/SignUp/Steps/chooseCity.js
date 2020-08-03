56659fa7365787ced95ba4aab696270cf198887c
MohammadAmin Deldari <contact.deldari@gmail.com>
2020-08-03 13:53:34 +0430

    re design select  inputs

--- src/screens/SignUp/Steps/chooseCity.js
+++ src/screens/SignUp/Steps/chooseCity.js
@@ -372,18 +372,17 @@ const styles = StyleSheet.create({
         paddingRight: 30, // to ensure the text is never behind the icon
     },
     inputAndroid: {
-        fontSize: 16,
+        fontSize: 13,
         paddingHorizontal: 10,
-        fontFamily: 'IRANSansWeb(FaNum)_Light',
+        fontFamily: 'IRANSansWeb(FaNum)_Medium',
         paddingVertical: 8,
-        height: 60,
+        height: 50,
         width: deviceWidth * 0.9,
-        paddingRight: 30, // to ensure the text is never behind the icon
     },
     iconContainer: {
-        left: 30,
-        top: 17,
-    },
+        left: 10,
+        top: 13,
+    }
 
 });
 const mapStateToProps = state => {
