544a0c9ba28fe2c01a29a952793e48dc51e14b4a
MohammadAmin Deldari <contact.deldari@gmail.com>
2020-09-07 13:14:43 +0430

    debug placeholder contents

--- src/screens/Requests/Requests.js
+++ src/screens/Requests/Requests.js
@@ -568,28 +568,26 @@ class Requests extends PureComponent {
                         keyboardDismissMode='on-drag'
                         keyboardShouldPersistTaps='handled'
                         ListEmptyComponent={() => !!buyAdRequestLoading ?
-                            [1, 2, 3, 4, 5, 6, 7, 8].map((_, index) =>
+                            [1, 2, 3, 4, 5].map((_, index) =>
                                 <View style={{
                                     backgroundColor: '#fff',
-                                    paddingVertical: 25,
+                                    paddingTop: 25,
+                                    paddingBottom: 10,
                                     borderBottomWidth: 2,
-                                    borderBottomColor: '#aaa'
+                                    borderBottomColor: '#ddd'
                                 }}>
                                     <ContentLoader
                                         speed={2}
                                         width={deviceWidth}
                                         height={deviceHeight * 0.24}
-                                        viewBox="0 0 340 152"
+                                        viewBox="0 0 340 160"
                                         backgroundColor="#f3f3f3"
                                         foregroundColor="#ecebeb"
                                     >
-                                        <Rect x="105" y="-1" rx="3" ry="3" width="140" height="18" />
-                                        <Rect x="155" y="75" rx="3" ry="3" width="53" height="15" />
-                                        <Rect x="215" y="75" rx="3" ry="3" width="72" height="15" />
-                                        <Rect x="46" y="75" rx="3" ry="3" width="100" height="15" />
-                                        <Rect x="11" y="38" rx="3" ry="3" width="140" height="16" />
-                                        <Rect x="159" y="38" rx="3" ry="3" width="173" height="16" />
-                                        <Rect x="21" y="112" rx="3" ry="3" width="301" height="39" />
+                                        <Rect x="50" y="37" rx="3" ry="3" width="242" height="20" />
+                                        <Rect x="85" y="3" rx="3" ry="3" width="169" height="20" />
+                                        <Rect x="22" y="119" rx="3" ry="3" width="299" height="30" />
+                                        <Rect x="116" y="74" rx="3" ry="3" width="105" height="20" />
                                     </ContentLoader>
                                 </View>)
                             : <View style={{
