9fd64e91b5e7c182c76aac4159ea73c717a093c5
MohammadAmin Deldari <contact.deldari@gmail.com>
2020-09-08 17:27:51 +0430

    add user contact list placeholder content

--- src/screens/Messages/index.js
+++ src/screens/Messages/index.js
@@ -10,6 +10,8 @@ import { REACT_APP_API_ENDPOINT_RELEASE } from 'react-native-dotenv';
 import { connect } from 'react-redux';
 import * as messagesActions from '../../redux/messages/actions';
 import { ScrollView } from 'react-native-gesture-handler';
+import ContentLoader, { Rect, Circle, Path } from "react-content-loader/native"
+
 import { deviceWidth, deviceHeight } from '../../utils/deviceDimenssions';
 import Jmoment from 'moment-jalaali';
 import ChatModal from './ChatModal';
@@ -235,15 +237,40 @@ class ContactsList extends React.Component {
                         <CardItem >
                             <Body >
                                 <FlatList
-                                    ListEmptyComponent={() => contactsListLoading ? <View></View> : searchText ?
-                                        <View style={{ flex: 1, height: deviceHeight, justifyContent: 'center', alignItems: 'center' }}>
-                                            <AntDesign size={135} name='contacts' color='#BEBEBE' />
-                                            <Text style={{ fontSize: 20, fontFamily: 'IRANSansWeb(FaNum)_Bold', color: '#7E7E7E' }}>{locales('labels.noContactFound')}</Text>
-                                        </View> :
-                                        <View style={{ flex: 1, height: deviceHeight, justifyContent: 'center', alignItems: 'center' }}>
-                                            <Entypo size={135} name='message' color='#BEBEBE' />
-                                            <Text style={{ fontSize: 20, fontFamily: 'IRANSansWeb(FaNum)_Bold', color: '#7E7E7E' }}>{locales('labels.noChatFound')}</Text>
-                                        </View>
+                                    ListEmptyComponent={() =>
+                                        contactsListLoading ?
+                                            [1, 2, 3, 4, 5].map((_, index) =>
+                                                <View
+                                                    key={index}
+                                                    style={{
+                                                        backgroundColor: '#fff',
+                                                    }}>
+                                                    <ContentLoader
+                                                        speed={2}
+                                                        width={deviceWidth}
+                                                        height={80}
+                                                        viewBox="0 0 480 80"
+                                                        backgroundColor="#f3f3f3"
+                                                        foregroundColor="#ecebeb"
+                                                    >
+                                                        <Rect x="158" y="11" rx="3" ry="3" width="183" height="15" />
+                                                        <Circle cx="398" cy="33" r="33" />
+                                                        <Rect x="191" y="44" rx="3" ry="3" width="147" height="13" />
+                                                        <Rect x="0" y="11" rx="3" ry="3" width="88" height="15" />
+                                                        <Rect x="2" y="77" rx="0" ry="0" width="436" height="2" />
+                                                    </ContentLoader>
+                                                </View>)
+
+
+                                            : searchText ?
+                                                <View style={{ flex: 1, height: deviceHeight, justifyContent: 'center', alignItems: 'center' }}>
+                                                    <AntDesign size={135} name='contacts' color='#BEBEBE' />
+                                                    <Text style={{ fontSize: 20, fontFamily: 'IRANSansWeb(FaNum)_Bold', color: '#7E7E7E' }}>{locales('labels.noContactFound')}</Text>
+                                                </View> :
+                                                <View style={{ flex: 1, height: deviceHeight, justifyContent: 'center', alignItems: 'center' }}>
+                                                    <Entypo size={135} name='message' color='#BEBEBE' />
+                                                    <Text style={{ fontSize: 20, fontFamily: 'IRANSansWeb(FaNum)_Bold', color: '#7E7E7E' }}>{locales('labels.noChatFound')}</Text>
+                                                </View>
                                     }
                                     ref={this.props.contactsListRef}
                                     refreshing={!!contactsListLoading}
