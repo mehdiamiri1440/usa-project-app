1b47a20983eaee7f94e9f40fc67488a9856ca4fa
mehdi amiri <knownmma@gmail.com>
2020-09-21 20:19:17 +0330

    changes all  done before on release of 1.1.2.997

--- src/router/router.js
+++ src/router/router.js
@@ -163,40 +163,40 @@ const App = (props) => {
 
   useEffect(() => {
 
-    // fetch('https://app-download.s3.ir-thr-at1.arvanstorage.com/buskool.json')
-    //   .then(res => {
-    //     res.text().then(result => {
-    //       const resultOfVersion = JSON.parse(result);
-    //       if (
-    //         RNAppUpdate.versionName.toString() !==
-    //         resultOfVersion.versionName.toString()
-    //       ) {
-    //         if (!resultOfVersion.forceUpdate) {
-    //           setUpdateModalFlag(true);
-    //           // Alert.alert(
-    //           //     'به روز رسانی',
-    //           //     'نسخه جدیدی موجود است. آیا تمایل به  بروز رسانی دارید ؟',
-    //           //     [
-    //           //         {
-    //           //             text: 'به روز رسانی',
-    //           //             onPress: () => navigationRef.current.navigate('UpgradeApp')
-    //           //         },
-    //           //         {
-    //           //             text: 'انصراف',
-    //           //             onPress: () => { },
-    //           //             style: 'cancel'
-    //           //         },
-    //           //     ],
-    //           // );
-    //         }
-    //         else {
-    //           navigationRef.current.navigate('UpgradeApp')
-    //         }
-    //       }
-    //     });
-    //   })
-    //   .catch(err => navigationRef.current.navigate('SignUp')
-    //   );
+    fetch('https://app-download.s3.ir-thr-at1.arvanstorage.com/buskool.json')
+      .then(res => {
+        res.text().then(result => {
+          const resultOfVersion = JSON.parse(result);
+          if (
+            RNAppUpdate.versionName.toString() !==
+            resultOfVersion.versionName.toString()
+          ) {
+            if (!resultOfVersion.forceUpdate) {
+              setUpdateModalFlag(true);
+              // Alert.alert(
+              //     'به روز رسانی',
+              //     'نسخه جدیدی موجود است. آیا تمایل به  بروز رسانی دارید ؟',
+              //     [
+              //         {
+              //             text: 'به روز رسانی',
+              //             onPress: () => navigationRef.current.navigate('UpgradeApp')
+              //         },
+              //         {
+              //             text: 'انصراف',
+              //             onPress: () => { },
+              //             style: 'cancel'
+              //         },
+              //     ],
+              // );
+            }
+            else {
+              navigationRef.current.navigate('UpgradeApp')
+            }
+          }
+        });
+      })
+      .catch(err => navigationRef.current.navigate('SignUp')
+      );
 
     remoteConfig()
       .setDefaults({
