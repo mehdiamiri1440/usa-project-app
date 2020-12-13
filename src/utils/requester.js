e7c0e0be17d6337d7fb633d8adf80e7ffeaf1eff
mehdi amiri <knownmma@gmail.com>
2020-12-13 11:47:24 +0330

    alternative base url added to project for when main base url fails on calling and returns status 522

--- src/utils/requester.js
+++ src/utils/requester.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
-import { REACT_APP_API_ENDPOINT_RELEASE } from '@env';
+import { REACT_APP_API_ENDPOINT_RELEASE, REACT_APP_API_ALTERNATIVE_ENDPOINT_RELEASE } from '@env';
 import RnRestart from 'react-native-restart';
 import * as authActions from '../redux/auth/actions';
 import configureStore from '../redux/configureStore';
 
-const getUrl = route => {
+const getUrl = (route, alternativeBaseUrl) => {
     // if (__DEV__) {
     //     if (!RNEmulatorCheck.isEmulator())
     //         return `${REACT_APP_API_ENDPOINT_REAL_DEVICE}/${route}`;
     // }
     // return `http://192.168.1.102:3030/${route}`;
 
-    return `${REACT_APP_API_ENDPOINT_RELEASE}/${route}`
+    return `${alternativeBaseUrl ? REACT_APP_API_ALTERNATIVE_ENDPOINT_RELEASE : REACT_APP_API_ENDPOINT_RELEASE}/${route}`
 
 };
 
@@ -58,23 +58,25 @@ const redirectToLogin = _ => {
 
 
 
-const refreshToken = (route, method, data, withAuth, headers) => {
-    axios.post(`${REACT_APP_API_ENDPOINT_RELEASE}/refresh-token`, undefined, { headers }).then(result => {
-        const token = result.data.token
-        AsyncStorage.setItem("@Authorization", token).then(_ => {
-            fetchAPI({ route, method, data, withAuth })
+const refreshToken = (route, method, data, withAuth, headers, alternativeBaseUrl) => {
+    axios.post(`${alternativeBaseUrl ? REACT_APP_API_ALTERNATIVE_ENDPOINT_RELEASE : REACT_APP_API_ENDPOINT_RELEASE}/refresh-token`,
+        undefined, { headers })
+        .then(result => {
+            const token = result.data.token
+            AsyncStorage.setItem("@Authorization", token).then(_ => {
+                fetchAPI({ route, method, data, withAuth })
+            })
         })
-    })
         .catch(_ => redirectToLogin())
 };
 
 
-export const fetchAPI = async ({ route, method = 'GET', data = {}, withAuth = true, params = null }) => {
+export const fetchAPI = async ({ route, method = 'GET', data = {}, withAuth = true, params = null, alternativeBaseUrl = false }) => {
     const headers = await getRequestHeaders();
     return new Promise((resolve, reject) => {
         axios
             .request({
-                url: getUrl(route),
+                url: getUrl(route, alternativeBaseUrl),
                 method,
                 headers,
                 data,
@@ -87,13 +89,20 @@ export const fetchAPI = async ({ route, method = 'GET', data = {}, withAuth = tr
             .catch(err => {
                 if (err.response && err.response.status === 401) {
                     if (err.response.data.refresh)
-                        refreshToken(route, method, data, withAuth, headers)
+                        refreshToken(route, method, data, withAuth, headers, alternativeBaseUrl)
                     else redirectToLogin()
                 }
 
 
                 if (err.response && err.response.status === 400) {
                     reject(err.response.data);
+                }
+
+                if (err.response && err.response.status === 400) {
+                    reject(err.response.data);
+                }
+                if (err.response && err.response.status === 522) {
+                    return fetchAPI(route, method, data, withAuth, params, true)
                 }
 
                 else {
