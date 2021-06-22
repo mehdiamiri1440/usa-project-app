96fd5a99b20093414ba9100be21c33964561c866
mehdi amiri <mohammadmehdiamiri20@gmail.com>
2021-06-22 16:15:09 +0430

    corrected

--- locales/fa-ir.js
+++ locales/fa-ir.js
@@ -158,7 +158,7 @@ export default {
     'labels.specialProducts': 'ویژه‌ها',
     'labels.registerRequest': 'ثبت درخواست',
     'labels.somethingWentWrong': 'متاسفانه به مشکلی برخورده‌ایم',
-    'labels.problemHappened': 'مشکلی رخ داده است .',
+    'labels.problemHappened': 'مشکلی رخ داده است.',
     'labels.pleaseRetry': 'لطفا مجددا تلاش بفرمایید.',
     'labels.promoteRegistrationModalDescription': 'برای دسترسی به خریداران طلایی ، شماره کاربران ، ثبت محصول بیشتر و دیگر امکانات ویژه باسکول ، عضویت خود را ارتقا دهید',
     'labels.myProducts': 'محصولات من',
