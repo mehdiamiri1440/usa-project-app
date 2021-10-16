f7e8c2151075f038baa19ed5af16eaf2abf71080
mehdi amiri <mohammadmehdiamiri20@gmail.com>
2021-10-16 12:05:33 +0330

    all the home routes now have unique id

--- src/screens/Home/HomeRoutes.js
+++ src/screens/Home/HomeRoutes.js
@@ -9,28 +9,25 @@ import MaterialCommunityIcons from 'react-native-vector-icons/dist/MaterialCommu
 
 
 export const homeRoutes = [
-    { label: 'titles.revenues', icon: <FontAwesome5 size={25} name='dollar-sign' color='#556080' />, name: 'UserFriends' },
-    { label: 'labels.yourWallet', icon: <FontAwesome5 size={25} name='wallet' color='#556080' />, name: 'Wallet' },
-    { label: 'labels.dashboard', icon: <FontAwesome5 size={25} name='chart-line' color='#556080' />, name: 'Dashboard' },
-    { label: 'titles.editProfile', icon: <FontAwesome5 size={25} name='user' solid color='#556080' />, name: 'EditProfile' },
-    { label: 'labels.suggestedBuyers', icon: <FontAwesome5 size={25} name='list-ul' color='#556080' />, name: 'SuggestedBuyers' },
-    { label: 'labels.myRequests', icon: <Fontisto size={25} name='list-1' color='#556080' />, name: 'MyRequests' },
-    { label: 'labels.suggestedSellers', icon: <FontAwesome5 size={25} name='list-ul' color='#556080' />, name: 'SpecialProducts' },
-    { label: 'labels.messages', icon: <Entypo size={25} name='message' color='#556080' />, name: 'Messages' },
+    { id: 1, label: 'titles.revenues', icon: <FontAwesome5 size={25} name='dollar-sign' color='#556080' />, name: 'UserFriends' },
+    { id: 2, label: 'labels.yourWallet', icon: <FontAwesome5 size={25} name='wallet' color='#556080' />, name: 'Wallet' },
+    { id: 3, label: 'labels.dashboard', icon: <FontAwesome5 size={25} name='chart-line' color='#556080' />, name: 'Dashboard' },
+    { id: 4, label: 'titles.editProfile', icon: <FontAwesome5 size={25} name='user' solid color='#556080' />, name: 'EditProfile' },
+    { id: 5, label: 'labels.suggestedBuyers', icon: <FontAwesome5 size={25} name='list-ul' color='#556080' />, name: 'SuggestedBuyers' },
+    { id: 6, label: 'labels.myRequests', icon: <Fontisto size={25} name='list-1' color='#556080' />, name: 'MyRequests' },
+    { id: 7, label: 'labels.suggestedSellers', icon: <FontAwesome5 size={25} name='list-ul' color='#556080' />, name: 'SpecialProducts' },
+    { id: 8, label: 'labels.messages', icon: <Entypo size={25} name='message' color='#556080' />, name: 'Messages' },
 
-    // { label: 'titles.referralListTitle', icon: <Entypo size={25} name='share' color='#556080' />, name: 'UserFriends' },
-    // { label: 'labels.guid', icon: <Entypo size={25} name='help' color='#556080' />, name: 'Guid' },
-    { label: 'labels.myProducts', icon: <Fontisto size={25} name='list-1' color='#556080' />, name: 'MyProducts' },
+    // { id:16,label: 'titles.referralListTitle', icon: <Entypo size={25} name='share' color='#556080' />, name: 'UserFriends' },
+    // { id:17,label: 'labels.guid', icon: <Entypo size={25} name='help' color='#556080' />, name: 'Guid' },
+    { id: 9, label: 'labels.myProducts', icon: <Fontisto size={25} name='list-1' color='#556080' />, name: 'MyProducts' },
 
-    { label: 'labels.myProfile', icon: <MaterialCommunityIcons size={25} name='account-arrow-left-outline' color='#556080' />, name: 'Profile' },
-    { label: 'labels.authentication', icon: <MaterialIcons size={25} name='verified-user' color='#556080' />, name: 'Authentication' },
-    { label: 'labels.promoteRegistration', icon: <FontAwesome size={25} name='arrow-up' color='#fff' />, name: 'PromoteRegistration' },
-    { label: 'titles.support', icon: <FontAwesome5 size={25} name='headset' color='#556080' />, name: 'ContactUs' },
-    {
-        label: 'labels.contactInfoShowingGuid', icon: <FontAwesome5
-            size={25} name='phone-square' color='#556080' />, name: 'ContactInfoGuid'
-    },
-    { label: 'labels.settings', icon: <AntDesign size={25} name='setting' color='#556080' />, name: 'Settings' },
+    { id: 10, label: 'labels.myProfile', icon: <MaterialCommunityIcons size={25} name='account-arrow-left-outline' color='#556080' />, name: 'Profile' },
+    { id: 11, label: 'labels.authentication', icon: <MaterialIcons size={25} name='verified-user' color='#556080' />, name: 'Authentication' },
+    { id: 12, label: 'labels.promoteRegistration', icon: <FontAwesome size={25} name='arrow-up' color='#fff' />, name: 'PromoteRegistration' },
+    { id: 13, label: 'titles.support', icon: <FontAwesome5 size={25} name='headset' color='#556080' />, name: 'ContactUs' },
+    { id: 14, label: 'labels.contactInfoShowingGuid', icon: <FontAwesome5 size={25} name='phone-square' color='#556080' />, name: 'ContactInfoGuid' },
+    { id: 15, label: 'labels.settings', icon: <AntDesign size={25} name='setting' color='#556080' />, name: 'Settings' },
 ];
 
 export const sellerSpecialRoutes = [
