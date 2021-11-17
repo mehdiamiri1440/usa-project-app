1a2c9e1c2af708a68478e29f59eb8474c44234e2
mehdi amiri <knownmma@gmail.com>
2021-11-17 09:46:32 +0330

    device screen added to device dimenssions

--- src/utils/deviceDimenssions.js
+++ src/utils/deviceDimenssions.js
@@ -1,4 +1,6 @@
 import { Dimensions } from 'react-native'
 export const deviceWidth = Dimensions.get("window").width;
 export const deviceHeight = Dimensions.get("window").height;
+export const screenWidth = Dimensions.get("screen").width;
+export const screenHeight = Dimensions.get("screen").height;
 
