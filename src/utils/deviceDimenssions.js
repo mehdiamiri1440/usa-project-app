62c3f3b9d00773b7b87d45eb6331508ad96c5e8b
mehdi amiri <knownmma@gmail.com>
2021-11-18 16:51:59 +0330

    scale added

--- src/utils/deviceDimenssions.js
+++ src/utils/deviceDimenssions.js
@@ -4,3 +4,13 @@ export const deviceHeight = Dimensions.get("window").height;
 export const screenWidth = Dimensions.get("screen").width;
 export const screenHeight = Dimensions.get("screen").height;
 
+const { width, height } = Dimensions.get('window');
+
+//Guideline sizes are based on standard ~5" screen mobile device
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+export const scale = (size) => (width / guidelineBaseWidth) * size;
+export const verticalScale = (size) => (height / guidelineBaseHeight) * size;
+export const moderateScale = (size, factor = 0.5) => size + (scale(size) - size) * factor;
+
