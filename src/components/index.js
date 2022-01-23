953902fb50937f5fd85fd0961f8fd28eedbc0cbb
mehdi amiri <knownmma@gmail.com>
2022-01-23 17:20:38 +0330

    buskool text input component created

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import TextInputComponent from './textInput';
+
+export {
+    TextInputComponent as BuskoolTextInput
+};
---
+++ src/components/textInput/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+    TextInput
+} from 'react-native';
+
+export default props => {
+    return (
+        <TextInput
+            {...props}
+        />
+    )
+}
